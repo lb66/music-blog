c89386dea23b0c2bb0b8479c96fda8efbf9bb591
lb66 <807242867@qq.com>
2020-09-06 21:03:43 +0800

    歌单

--- cloudfunctions/getPlaylist/index.js
+++ cloudfunctions/getPlaylist/index.js
@@ -2,15 +2,29 @@
 const cloud = require('wx-server-sdk')
 
 cloud.init()
-
+const db = cloud.database()
+const rp = require('request-promise')
+const URL = 'http://neteasecloudmusicapi.zhaoboy.com/personalized?limit=60'
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+  const playlist = await rp(URL).then((res) => {
+    return JSON.parse(res).result
+  })
+  await db.collection('playlist').where({
+    highQuality: false
+  }).remove()
+  for (let i = 0, len = playlist.length; i < len; i++) {
+    await db.collection('playlist').add({
+      data: {
+        ...playlist[i],
+        createTime: db.serverDate()
+      }
+    }).then((res) => {
+      console.log('success')
+    }, (err) => {
+      console.log('fail')
+    })
   }
+
+
 }
