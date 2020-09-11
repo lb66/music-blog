7bea674640464121376b38009537d60a4fb41b5f
lb66 <807242867@qq.com>
2020-09-11 19:47:27 +0800

    上传图片到云存储

--- miniprogram/pages/blog-edit/blog-edit.js
+++ miniprogram/pages/blog-edit/blog-edit.js
@@ -1,5 +1,9 @@
 // pages/blog-edit/blog-edit.js
-const Max_Num = 140
+const Max_WordsNum = 140
+const Max_ImageNum = 9
+
+let content = ''
+let userInfo = {}
 Page({
 
   /**
@@ -7,22 +11,25 @@ Page({
    */
   data: {
     wordsNum: '最大字数：140',
-    footerBottom: 0
+    footerBottom: 0,
+    images: [],
+    showSelect: true
   },
-  onFocus(event){
+  onFocus(event) {
     //获取键盘高度
     this.setData({
-      footerBottom:event.detail.height
+      footerBottom: event.detail.height
     })
   },
-  onBlur(){
+  onBlur() {
     this.setData({
-      footerBottom:0
+      footerBottom: 0
     })
   },
   onInput(event) {
+    content = event.detail.value
     let num = event.detail.value.length
-    if (num < Max_Num) {
+    if (num < Max_WordsNum) {
       this.setData({
         wordsNum: '当前字数：' + num
       })
@@ -32,11 +39,92 @@ Page({
       })
     }
   },
+  onSelectImage() {
+    let num = Max_ImageNum - this.data.images.length
+    wx.chooseImage({
+      count: num,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        // tempFilePath可以作为img标签的src属性显示图片
+        this.setData({
+          images: this.data.images.concat(res.tempFilePaths)
+        })
+        num = Max_ImageNum - this.data.images.length
+        if (num <= 0) {
+          this.setData({
+            showSelect: false
+          })
+        }
+      }
+    })
+
+  },
+  onDeleteImage(event) {
+    this.data.images.splice(event.target.dataset.index, 1)
+    this.setData({
+      images: this.data.images,
+      showSelect: true
+    })
+  },
+  onPreviewImage(event) {
+    wx.previewImage({
+      urls: this.data.images,
+      current: event.target.dataset.imgSrc
+    })
+  },
+  onPublish() {
+    wx.showLoading({
+      title: '上传中',
+    })
+    let promiseArr = []
+    let fileIds = []
+    for (let i = 0; i < this.data.images.length; i++) {
+      let p = new Promise((resolve, reject) => {
+        let image = this.data.images[i]
+        let suffix = /\.\w+$/.exec(image)[0]
+        wx.cloud.uploadFile({
+          cloudPath: 'blog/' + Date.now() + Math.floor(Math.random() * 1000) + suffix,
+          filePath: image, // 文件路径
+        }).then(res => {
+          fileIds = fileIds.concat(res.fileID)
+          resolve()
+        }).catch(err => {
+          reject()
+        })
+      })
+      promiseArr.push(p)
+    }
+    Promise.all(promiseArr).then((res) => {
+      wx.cloud.database().collection('blog').add({
+        data: {
+          ...userInfo,
+          content,
+          img: fileIds,
+          createTime: wx.cloud.database().serverDate()
+        }
+      }).then((res) => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '发表成功',
+        })
+        wx.navigateBack()
+      })
+    }).catch((err) => {
+      console.log('fail')
+      wx.hideLoading()
+      wx.showToast({
+        title: '发表失败',
+        icon: 'none'
+      })
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     console.log(options)
+    userInfo = options
   },
 
   /**
